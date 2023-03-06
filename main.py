242fa6d7d7bd44f55e724ddd6d36fc4e7b717939
tcosyn <82354053+tcosyn@users.noreply.github.com>
2023-03-06 05:21:48 -0800

    Add files via upload
    Reworked algorithm:
    - Applied subpixel to contours then cast as an int for drawing
    - Changed RETR_LIST to RETR_EXTERNAL for the use of orderedCorners function (orders corners clockwise)
    - Added morphological opening to clean warped image.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import cv2
 import numpy as np
-from scipy.spatial import distance as dist
 
 # Define a video capture object
 captureCam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Set resolution to 720p
-captureCam.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-captureCam.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-captureCam.set(cv2.CAP_PROP_FPS, 60)
+captureCam.set(3, 1280)
+captureCam.set(4, 720)
 
 # Set font for display
 font = cv2.FONT_HERSHEY_COMPLEX
@@ -29,6 +27,7 @@ centerFrame = (centerW, centerH)
 # Print camera information
 print(f'Resolution: {camW} x {camH}')
 print(f'FPS: {fps}')
+
 
 
 def resizeWarp(img, square_size):
@@ -77,6 +76,9 @@ def matchBIT(contour):
     # Resize contour to
     cnt1 = cv2.resize(contour, dim1, interpolation=cv2.INTER_AREA)
 
+    # disp = cv2.resize(contour, (500,500), interpolation=cv2.INTER_AREA)
+    # cv2.imshow('resized',disp)
+
     # Convert image data to binary data
     for i in range(dim):
         for j in range(dim):
@@ -89,6 +91,7 @@ def matchBIT(contour):
     cropped_bin3 = cv2.rotate(cropped_bin2, cv2.ROTATE_90_CLOCKWISE)
     cropped_bin4 = cv2.rotate(cropped_bin3, cv2.ROTATE_90_CLOCKWISE)
 
+    print(cropped_bin1)
     # Convert from 2D to 1D array
     cropped_bin1 = cropped_bin1.reshape(-1)
     cropped_bin2 = cropped_bin2.reshape(-1)
@@ -107,11 +110,11 @@ def matchBIT(contour):
     if (matchRating1 > threshold) | (matchRating2 > threshold) | (matchRating3 > threshold) | (
             matchRating4 > threshold):
         print(f"MARKER MATCH! Match:{max(matchRating1, matchRating2, matchRating3, matchRating4)}\n\n")
-        if (matchRating1 > matchRating2 and matchRating1 > matchRating3 and matchRating1 > matchRating4):
+        if matchRating1 > matchRating2 and matchRating1 > matchRating3 and matchRating1 > matchRating4:
             return True, 0
-        elif (matchRating2 > matchRating1 and matchRating2 > matchRating3 and matchRating2 > matchRating4):
+        elif matchRating2 > matchRating1 and matchRating2 > matchRating3 and matchRating2 > matchRating4:
             return True, 1
-        elif (matchRating3 > matchRating1 and matchRating3 > matchRating2 and matchRating3 > matchRating4):
+        elif matchRating3 > matchRating1 and matchRating3 > matchRating2 and matchRating3 > matchRating4:
             return True, 2
         else:
             return True, 3
@@ -119,8 +122,10 @@ def matchBIT(contour):
         return False, 999
 
 
+# returns corners ordered in clockwise order
 def orderCornersN(crnrs):
     # Create copy of corners
+    crnrs = crnrs.reshape(4, 2)
     ordrd_crnrs = crnrs
 
     # Calculate center point
@@ -135,6 +140,7 @@ def orderCornersN(crnrs):
         ordrd_crnrs[[0, 1]] = crnrs[[1, 0]]
         ordrd_crnrs[[2, 3]] = crnrs[[3, 2]]
 
+    ordrd_crnrs = ordrd_crnrs.reshape(4, 1, 2)
     return ordrd_crnrs
 
 
@@ -144,110 +150,111 @@ while captureCam.isOpened():
 
     if ret:  # If frame is opened
         # Gaussian blur image with kernel size of 5 x 5 & sigmaY = 0
-        frame = cv2.GaussianBlur(frame, (5, 5), 0)
+        gBlur = cv2.GaussianBlur(frame, (5, 5), 0)
 
         # Grayscale image
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(gBlur, cv2.COLOR_BGR2GRAY)
 
         # Adaptive threshold image using gaussian average w/ region size of 11 x 11 and 5 offset
+        # thresh = cv2.adaptiveThreshold(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)
         thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)
 
         thresh = ~thresh  # Invert thresholded image
 
         # Find contours in image using simple approximation
-        contours, _ = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # Sort contours by area from least to greatest
-        contours = sorted(contours, key=lambda x: cv2.contourArea(x))  # REPLACE WITH CHILD/PARENT CONTOUR DELETION
+        contours = sorted(contours, key=lambda x: cv2.contourArea(x),
+                          reverse=True)  # REPLACE WITH CHILD/PARENT CONTOUR DELETION
 
         for cnt in contours:
             # Reduce complexity of contours to reduce amount of points
-            approx = cv2.approxPolyDP(cnt, 0.05 * cv2.arcLength(cnt, True), True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
 
-            if len(approx) == 4:  # If number of sides equals four
+            if len(approx) == 4 and cv2.isContourConvex(approx):  # If number of sides equals four & is convex
                 area = cv2.contourArea(approx)  # Get area of contour
 
                 # If contour is within threshold and is convex
-                if 10000.0 < area < 600000 and cv2.isContourConvex(approx):
+                if 700.0 < area < 600000:
+                    # Draw rejected contours
+                    approx = np.float32(approx)
+                    approx = cv2.cornerSubPix(gray, approx, (5, 5), (-1, -1), criteria)
 
-                    # cv2.drawContours(frame, [approx], 0, (0, 0, 255), 2)
+                    # CHECKME = approx
+                    approx = orderCornersN(approx)
 
-                    rect = cv2.minAreaRect(approx)  # Create bounding rectangle with minimum bounding area
-                    box = cv2.boxPoints(rect)  # Convert Box2D into set of corners for drawing
-                    box = np.intp(box)  # Cast as int
-
-                    (x, y, w, h) = cv2.boundingRect(cnt)  # Get bounding rectangle for contour extraction
-                    crop_img = frame[y:y + h, x:x + w]  # Extract frame with rectangle
-                    src_pts = box.astype("float32")  # Cast as float32 & set source points
-
-                    # Set destination points
-                    dst_pts = np.array([[0, h - 1], [0, 0], [w - 1, 0], [w - 1, h - 1]], dtype="float32")
+                    dst_pts = np.array([[0, 0], [48, 0], [48, 48], [0, 48]], dtype="float32")
 
                     # Get perspective transform
-                    M = cv2.getPerspectiveTransform(src_pts, dst_pts)
+                    M = cv2.getPerspectiveTransform(approx, dst_pts)
 
                     # Warp image from perspective transform
-                    warped = cv2.warpPerspective(frame, M, (w, h))
+                    warped = cv2.warpPerspective(frame, M, (49, 49))
+                    warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
 
-                    # Convert warped image to binary
-                    _, warped_bin = cv2.threshold(warped, 128, 255, cv2.THRESH_BINARY)
+                    # warpbin = cv2.adaptiveThreshold(warped, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 5)
+                    _, warpbin = cv2.threshold(warped, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-                    # Resize warped image
-                    rWarp = resizeWarp(warped, 5.0)
+                    # Resize image and show perspective transformed image
+                    resized_up = cv2.resize(warpbin, (490, 490), interpolation=cv2.INTER_NEAREST)
+                    cv2.imshow("warped", resized_up)
 
-                    # Resize warped image with square size 5
-                    rWarp = resizeWarp(warped, 5.0)
+                    # Remove small artifacts from the image by morphologically opening the image w/ a 3 x 3 kernel
+                    warpbin = cv2.erode(warpbin, np.ones((3, 3)))
+                    warpbin = cv2.dilate(warpbin, np.ones((3, 3)))
+
+                    # Resize image and show cleaned image
+                    resized_up = cv2.resize(warpbin, (490, 490), interpolation=cv2.INTER_NEAREST)
+                    cv2.imshow("opened", resized_up)
 
                     # Check if square is marker
-                    isMarker, rotVal = matchBIT(rWarp)
+                    print("checking")
+                    isMarker, rotVal = matchBIT(warpbin)
 
                     if isMarker:
+                        approxINT = np.intp(approx)
+
                         # Outline contour as green in frame
-                        cv2.drawContours(frame, [approx], 0, (0, 255, 0), 2)
-
-                        # Convert Box2D into set of corners (float32) for drawing
-                        corner = cv2.boxPoints(rect)
-
-                        # Refine corner positions to subpixel level
-                        corners = cv2.cornerSubPix(gray, corner, (5, 5), (-1, -1), criteria)
+                        cv2.drawContours(frame, [approxINT], 0, (0, 255, 0), 1)
 
                         # Calculate center point
-                        center_x = (corners[0][0] + corners[1][0] + corners[2][0] + corners[3][0]) / 4.0
-                        center_y = (corners[0][1] + corners[1][1] + corners[2][1] + corners[3][1]) / 4.0
+                        center_x = (approx[0][0][0] + approx[1][0][0] + approx[2][0][0] + approx[3][0][0]) / 4.0
+                        center_y = (approx[0][0][1] + approx[1][0][1] + approx[2][0][1] + approx[3][0][1]) / 4.0
 
-                        center_x = np.int0(center_x)  # Cast as int
-                        center_y = np.int0(center_y)  # Cast as int
                         center = (center_x, center_y)  # Save coordinates
+                        center = np.intp(center)
 
                         # Draw center point
                         cv2.circle(frame, center, 2, (125, 125, 125), 2)
 
                         # Draw line from center point to center of marker
-                        cv2.line(frame, center, centerFrame, (191, 0, 255), 2)
+                        cv2.line(frame, center, centerFrame, (191, 0, 255), 1)
 
                         # Calculate offset from the center
                         offsetx = center_x - centerW
                         offsety = centerH - center_y
 
                         # Create string to write on frame
-                        STR = str(offsetx) + ", " + str(offsety)
+                        STR = str(int(offsetx)) + ", " + str(int(offsety))
 
                         # Write offset from center at center of frame
                         cv2.putText(frame, STR, centerFrame, font, 0.5, (255, 0, 0))
 
-                        newCrnrs = orderCornersN(corners)  # Order corners
-                        cornersINT = np.int0(newCrnrs)  # Cast as int
-
                         # Draw Corners
-                        cv2.circle(frame, cornersINT[0].ravel(), 2, (255, 0, 0), 5)
-                        cv2.circle(frame, cornersINT[1].ravel(), 2, (0, 255, 0), 5)
-                        cv2.circle(frame, cornersINT[2].ravel(), 2, (0, 0, 255), 5)
-                        cv2.circle(frame, cornersINT[3].ravel(), 2, (0, 255, 255), 5)
+                        cv2.circle(frame, approxINT[0].ravel(), 2, (255, 0, 0), 5)
+                        cv2.circle(frame, approxINT[1].ravel(), 2, (0, 255, 0), 5)
+                        cv2.circle(frame, approxINT[2].ravel(), 2, (0, 0, 255), 5)
+                        cv2.circle(frame, approxINT[3].ravel(), 2, (0, 255, 255), 5)
 
                         # Write amount of rotations on top left corner
-                        cv2.putText(frame, str(rotVal), cornersINT[0].ravel(), font, 0.5, (255, 0, 0))
+                        cv2.putText(frame, str(rotVal), approxINT[0].ravel(), font, 0.5, (255, 0, 0))
 
+
+                        print(approx)
                         break
+                    else:
+                        cv2.drawContours(frame, [np.intp(approx)], 0, (0, 0, 255), 1)
 
         cv2.imshow("Webcam", frame)  # stream with corner detected points
         key = cv2.waitKey(1)
@@ -256,4 +263,5 @@ while captureCam.isOpened():
     else:
         break
 
+captureCam.release()
 cv2.destroyAllWindows()
